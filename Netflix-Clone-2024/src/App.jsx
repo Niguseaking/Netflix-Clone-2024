05327346cdfc69fc84c8a8c63d98fb37b9f89a2e
Nigusea  Dessie <wubsis12@gmail.com>
2024-11-22 20:05:44 -0600

    header and Footer

--- Netflix-Clone-2024/src/App.jsx
+++ Netflix-Clone-2024/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './Pages/Home/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-       <h1> hello world</h1>
+    
+      <div className='App'>
+       < Home/>
       </div>
       
-    </>
+    
   )
 }
 
---
+++ Netflix-Clone-2024/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Component/Header/Header'
+import Footer from '../../Component/Footer/Footer'
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home

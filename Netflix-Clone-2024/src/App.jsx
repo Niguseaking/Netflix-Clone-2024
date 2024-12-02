e518496b2494126e54f9b6338fd72770ebc73eb8
Nigusea  Dessie <wubsis12@gmail.com>
2024-12-02 16:01:46 -0600

    last update

--- Netflix-Clone-2024/src/App.jsx
+++ Netflix-Clone-2024/src/App.jsx
@@ -1,22 +1,4 @@
-// import { useState } from 'react'
-// import './App.css'
-// import Home from './Pages/Home/Home'
-// import
-// function App() {
-//   const [count, setCount] = useState(0)
 
-//   return (
-    
-//       <div className='App'>
-//        < Home/>
-//       </div>
-      
-    
-//   )
-
-
-// export default App
-// // }
 import { useState } from 'react'
 import './App.css'
 import Home from '././Pages/Home/Home';

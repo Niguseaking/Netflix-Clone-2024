af91fa9547336378809cd5b4bb6c6edbf531a11c
Nigusea  Dessie <wubsis12@gmail.com>
2024-12-02 16:45:10 -0600

    Configure for GitHub Pages deployment

--- Netflix-Clone-2024/vite.config.js
+++ Netflix-Clone-2024/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Netflix-Clone-2024/', // Replace <repository-name> with your GitHub repository name
 })
+

d7891d1dd291cdb8c790a12fceeca226405f4d86
Nigusea  Dessie <wubsis12@gmail.com>
2025-01-04 20:32:52 -0600

    last update

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,22 @@
 // })
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-});
-
+})

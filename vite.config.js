55e32b5d60daf7d663ecd74830efc8242d6bb478
Nigusea  Dessie <wubsis12@gmail.com>
2025-01-04 19:56:59 -0600

    Fix axios module path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Netflix-Clone-2024/', // Replace <repository-name> with your GitHub repository name
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix-Clone-2024/', // Replace <repository-name> with your GitHub repository name
-})
-
+});
